be3f7ac23057a9cd309ef7684d748e441eb5a17e
Partner <gpartnerllc@gmail.com>
2026-08-23 20:21:01 -0300

    Delete types.d.ts

--- types.d.ts
+++
@@ -1,5 +0,0 @@
-export interface Env {
-  DB: D1Database;
-  /** Opcional: token para proteger endpoints administrativos, definido via `wrangler secret put ADMIN_TOKEN` */
-  ADMIN_TOKEN?: string;
-}
